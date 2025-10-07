98d3a4f1437c30fa4a3179f84001343b2b2717af
Fred <97324318+clouxx@users.noreply.github.com>
2025-10-06 22:41:05 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'theme/app_theme.dart';
 import 'widgets/petfy_widgets.dart';
-// 👇 importa tu pantalla
+// 👇 importa tu pantalla*
 import 'ui/home/petfy_home.dart';
 
 
