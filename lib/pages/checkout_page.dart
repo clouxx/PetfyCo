5449542a87712c67c435a4ba92d904c917e6a5d6
falandete <f.alandete@uniandes.edu.co>
2026-05-28 19:29:45 -0500

    fix: add missing go_router import in checkout_page (build error)

--- lib/pages/checkout_page.dart
+++ lib/pages/checkout_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../config/app_config.dart';
